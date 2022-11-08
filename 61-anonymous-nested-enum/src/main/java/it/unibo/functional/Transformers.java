81ff71e5774560ba8f3e6cf1f8e7639189f2d093
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2022-11-08 16:54:39 +0100

    Implement class' methods

--- 61-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
+++ 61-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
@@ -54,7 +54,12 @@ public final class Transformers {
      * @param <O> output elements type
      */
     public static <I, O> List<O> transform(final Iterable<I> base, final Function<I, O> transformer) {
-        return null;
+        return flattenTransform(base, new Function<I, List<O>>() {
+            @Override
+            public List<O> call(final I input) {
+                return List.of(transformer.call(input));
+            }
+        });
     }
 
     /**
@@ -70,7 +75,7 @@ public final class Transformers {
      * @param <I> type of the collection elements
      */
     public static <I> List<? extends I> flatten(final Iterable<? extends Collection<? extends I>> base) {
-        return null;
+        return flattenTransform(base, Function.identity());
     }
 
     /**
@@ -87,7 +92,19 @@ public final class Transformers {
      * @param <I> elements type
      */
     public static <I> List<I> select(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        return flattenTransform(base, new Function<I,Collection<? extends I>>() {
+
+            @Override
+            public Collection<? extends I> call(I input) {
+                if (test.call(input)) {
+                    return List.of(input);
+                }
+                else {
+                    return List.of();
+                }
+            }
+            
+        });
     }
 
     /**
@@ -103,6 +120,13 @@ public final class Transformers {
      * @param <I> elements type
      */
     public static <I> List<I> reject(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        return select(base, new Function<I, Boolean>() {
+
+            @Override
+            public Boolean call(I input) {
+                return !test.call(input);
+            }
+             
+        });
     }
 }
